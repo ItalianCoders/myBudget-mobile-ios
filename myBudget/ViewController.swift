a62479ef27b2fa62544c6038df4f1d92d08252b9
IThoughtUGNU <ruggiero.altini@gmail.com>
2018-04-18 22:15:44 +0200

    Initial Commit

---
+++ myBudget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myBudget
+//
+//  Created by Ruggiero Altini on 18/04/18.
+//  Copyright © 2018 Ruggiero Altini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
